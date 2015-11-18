d8cd78a6b29e2518393eb0b93d21a1a93e00d5d5
Xhriman <seal789ie@gmail.com>
2015-11-18 13:29:02 +0800

    remove unused

--- Assets/Scripts/AttackerController.cs
+++ Assets/Scripts/AttackerController.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class AttackerController : MonoBehaviour {
 
+	private Animator anim;
+	private AnimatorStateInfo currentState;
+	private AnimatorStateInfo previousState;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
