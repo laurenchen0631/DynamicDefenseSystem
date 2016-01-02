b4311048254b2b620072b1546343ba9e89a1e257
Xhriman <seal789ie@gmail.com>
2016-01-02 23:26:08 +0800

    update

--- Assets/Scripts/DefenserController.cs
+++ Assets/Scripts/DefenserController.cs
@@ -1,22 +1,171 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+
+//struct BodyJoints {
+//	public Transform hips;
+//	public Transform spine;
+//	public Transform neck;
+//	public Transform head;
+//
+//	public Transform leftShoulder;
+//	public Transform leftUpperArm;
+//	public Transform leftLowerArm;
+//	public Transform leftHand;
+//	public Transform leftIndexProximal;
+//
+//	public Transform rightShoulder;
+//	public Transform rightUpperArm;
+//	public Transform rightLowerArm;
+//	public Transform rightHand;
+//	public Transform rightIndexProximal;
+//}
+
+public struct Joint {
+	public Vector3 position;
+	public Quaternion rotation;
+}
 
 public class DefenserController : MonoBehaviour {
 
 	public Animator enemy;
+
+	public bool isRecording = false;
+	public bool isReacting = false;
+	public Button record;
+
+	public int numJoints = 18;
+	protected List<Joint[]> bones = new List<Joint[]>();
 //	private HumanBodyBones enemyJoints;
 
-	// Use this for initialization
+	void Awake() {
+		if (numJoints > 18) {
+			numJoints = 18;
+		}
+	}
+
 	void Start () {
-//		enemy = 
-//		Animator anmi = enemy.GetComponents<Animator> () as Animator;
-//		enemyJoints 
+		record.image.color =  Color.white;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (enemy.GetBoneTransform (boneIndex[0]).position);
+		if (isRecording) {
+			Joint[] joints = new Joint[numJoints];
+			for (int i = 0; i < numJoints; i++) {
+				Transform tmp = enemy.GetBoneTransform (boneIndex [i]);
+				joints [i] = new Joint {
+					position = new Vector3 (tmp.position.x, tmp.position.x, tmp.position.z),
+					rotation = new Quaternion (tmp.rotation.x, tmp.rotation.y, tmp.rotation.z, tmp.rotation.w),
+				};
+			}
+			bones.Add (joints);
+		}
+	}
+
+	void LateUpdate() {
+//		Transform trans = enemy.GetBoneTransform (boneIndex [4]) ;
+//		trans.rotation = new Quaternion ();
+
+		if (isReacting) {
+			isReacting = false;
+		
+			StartCoroutine ("Playback");
+		}
+	}
+
+	public void toggleRecord() {
+		if (isRecording) {
+			record.image.color =  Color.white;
+			Text[] textEle = record.GetComponentsInChildren<Text>();
+			textEle[0].text = "Record";
+
+			isRecording = false;
+			startAnalysis ();
+		} else {
+			bones.Clear ();
+			isRecording = true;
+			record.image.color =  Color.red;
+			Text[] textEle = record.GetComponentsInChildren<Text>();
+			textEle[0].text = "Recording...";
+		}
+	}
+
+	private void startAnalysis() {
+		attackPathAnalysis ();
+		defenseStrategyAnalysis ();
+	}
+
+	private void attackPathAnalysis() {
+
+		// 1. Find enemy attack from right or left
+
+		// 2. Find the coordinate that attacked. (coordinate origin is ???)
+
+		// 3. Find the parts that will be attacked.
+		foreach (Joint[] joints in bones) {
+//			Debug.Log ("LeftShoulder" + joints [4].rotation.eulerAngles);
+			Debug.Log ("LeftShoulder" + joints [4].position);
+
+		}
+
+		foreach (Joint[] joints in bones) {
+//			Debug.Log ("LeftUpperArm" + joints [5].rotation.eulerAngles);
+			Debug.Log ("LeftUpperArm" + joints [5].position);
+		}
+
+		foreach (Joint[] joints in bones) {
+//			Debug.Log ("LeftLowerArm" + joints [6].rotation.eulerAngles);
+			Debug.Log ("LeftLowerArm" + joints [6].position);
+		}
+
+		foreach (Joint[] joints in bones) {
+//			Debug.Log ("LeftHand" + joints [7].rotation.eulerAngles);
+			Debug.Log ("LeftHand" + joints [7].position.x);
+		}
+
+//		foreach (Joint[] joints in bones) {
+//			Debug.Log ("LeftIndexProximal" + joints [7].rotation.eulerAngles);
+//		}
+
+		foreach(Joint[] joints in bones) {
+//			Debug.Log ("LeftShoulder" + joints [4].rotation.eulerAngles);
+//			Debug.Log ("LeftUpperArm" + joints [5].rotation.eulerAngles);
+//			Debug.Log ("LeftLowerArm" + joints [6].rotation.eulerAngles);
+//			Debug.Log ("RightHand" + joints [7].rotation.eulerAngles);
+//			Debug.Log ("LeftIndexProximal" + joints [8].rotation.eulerAngles);
+
+			//			for (int j = 0; j < joints.Length; j++) {
+			//				Transform trans = enemy.GetBoneTransform (boneIndex [j]) ;
+			//				trans.rotation = new Quaternion ();
+			//
+			//				trans.position = joints [j].position;
+			//				trans.rotation = joints [j].rotation;
+			//			}
+		}
+	}
+
+	private void defenseStrategyAnalysis() {
+		
+	}
+
+	IEnumerator Playback ()
+	{
+		foreach(Joint[] joints in bones) {
+			for (int j = 0; j < joints.Length; j++) {
+				Transform trans = enemy.GetBoneTransform (boneIndex [j]) ;
+				trans.rotation = new Quaternion ();
+
+				trans.position = joints [j].position;
+				trans.rotation = joints [j].rotation;
+			}
+
+			Debug.Log (joints [5].rotation.eulerAngles);
+			Debug.Log (enemy.GetBoneTransform (boneIndex [5]).rotation.eulerAngles);
+
+			yield return null;
+		}
 
 	}
 
@@ -31,22 +180,22 @@ public class DefenserController : MonoBehaviour {
 		{5, HumanBodyBones.LeftUpperArm}, 
 		{6, HumanBodyBones.LeftLowerArm}, 
 		{7, HumanBodyBones.LeftHand}, 
-		{8, HumanBodyBones.LeftIndexProximal},
+//		{8, HumanBodyBones.LeftIndexProximal},
 
-		{9, HumanBodyBones.RightShoulder},
-		{10, HumanBodyBones.RightUpperArm},
-		{11, HumanBodyBones.RightLowerArm},
-		{12, HumanBodyBones.RightHand},
-		{13, HumanBodyBones.RightIndexProximal},
+		{8, HumanBodyBones.RightShoulder},
+		{9, HumanBodyBones.RightUpperArm},
+		{10, HumanBodyBones.RightLowerArm},
+		{11, HumanBodyBones.RightHand},
+//		{13, HumanBodyBones.RightIndexProximal},
 
-//		{14, HumanBodyBones.LeftUpperLeg},
-//		{15, HumanBodyBones.LeftLowerLeg},
-//		{16, HumanBodyBones.LeftFoot},
-//		{17, HumanBodyBones.LeftToes},
-//
-//		{18, HumanBodyBones.RightUpperLeg},
-//		{19, HumanBodyBones.RightLowerLeg},
-//		{20, HumanBodyBones.RightFoot},
-//		{21, HumanBodyBones.RightToes},
+		{12, HumanBodyBones.LeftUpperLeg},
+		{13, HumanBodyBones.LeftLowerLeg},
+		{14, HumanBodyBones.LeftFoot},
+//		{15, HumanBodyBones.LeftToes},
+
+		{15, HumanBodyBones.RightUpperLeg},
+		{16, HumanBodyBones.RightLowerLeg},
+		{17, HumanBodyBones.RightFoot},
+//		{19, HumanBodyBones.RightToes},
 	};
 }
